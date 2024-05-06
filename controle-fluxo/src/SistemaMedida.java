adf50c7c7b7e85ba04da46b03518e5492de71e31
lais <laismtelles@gmail.com>
2024-05-06 10:48:40 -0300

    Conteúdo de estruturas condicionais java

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,15 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            } case "M": {
+                System.out.println("Whtasapp e Instagram grártis");
+            } case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
